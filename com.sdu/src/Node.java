959b5a8ab44ba932bef6cbf4a39a9d49ba81efe7
yh1070 <1187603543@qq.com>
2024-07-29 00:14:51 +0800

    debug

---
+++ com.sdu/src/Node.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//public class Node {
+//    int val;
+//    Node next;
+//    Node random;
+//
+//    public Node(int val) {
+//        this.val = val;
+//        this.next = null;
+//        this.random = null;
+//    }
+//}
+class Node {
+    public int val;
+    public List<Node> neighbors;
+
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
