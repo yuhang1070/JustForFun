777a1a57d9a1d15be259b3855bd5c98b02672432
yh1070 <1187603543@qq.com>
2024-07-29 00:57:54 +0800

    debug

--- com.sdu/src/Main.java
+++ com.sdu/src/Main.java
@@ -1,2 +1,42 @@
-public class Main {
+import java.util.Arrays;
+
+class Solution {
+    public int minimumMountainRemovals(int[] nums) {
+        int[] preArr = getPreArr(nums);
+        int[] sufArr = getSufArr(nums);
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (preArr[i] > 1 && sufArr[i] > 1) {
+                ans = Math.max(ans, preArr[i] + sufArr[i] - 1);
+            }
+        }
+        return nums.length - ans;
+    }
+
+    private int[] getPreArr(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp;
+    }
+
+    private int[] getSufArr(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        for (int i = nums.length - 2; i > -1; i--) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp;
+    }
+
 }
